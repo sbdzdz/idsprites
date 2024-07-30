73e1f920df8a5bc1b859263b50bfceef0abe92b3
Sebastian <sebastian.dziadzio@gmail.com>
2024-07-30 19:13:21 +0200

    Remove triplets from test

--- tests/test_infinite_dsprites.py
+++ tests/test_infinite_dsprites.py
@@ -3,10 +3,7 @@
 import pytest
 import numpy as np
 
-from idsprites import (
-    InfiniteDSprites,
-    InfiniteDSpritesTriplets,
-)
+from idsprites import InfiniteDSprites
 
 
 @pytest.mark.parametrize(
@@ -66,19 +63,6 @@ def test_idsprites_image(img_size, color_range, grayscale, expected_shape):
     assert image.shape == expected_shape
     assert image.min() >= 0.0
     assert image.max() <= 1.0
-
-
-@pytest.mark.parametrize(
-    "img_size,color_range,expected_shape",
-    [(244, ["white"], (3, 244, 244)), (256, ["red"], (3, 256, 256))],
-)
-def test_idsprites_triplets_image(img_size, color_range, expected_shape):
-    """Test that the dataset can be iterated over."""
-    dataset = InfiniteDSpritesTriplets(img_size=img_size, color_range=color_range)
-    images, _ = next(iter(dataset))
-    assert images[0].shape == expected_shape
-    assert images[0].min() >= 0.0
-    assert images[0].max() <= 1.0
 
 
 @pytest.mark.parametrize(
